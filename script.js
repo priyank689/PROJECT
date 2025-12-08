15eef5ae0d5edc2c901d1ec737197230f6f34c0a
PRIYANSHU <priyank2657@gmail.com>
2025-12-08 20:44:46 +0530

    created a javascript page and added some codes

---
+++ script.js
@@ -0,0 +1,32 @@
+// MENU SECTION
+
+let all = document.getElementById("all-navbar");
+let all1=document.getElementById("nav-burger");
+let all2=document.getElementById("nav-drinks");
+let all3=document.getElementById("nav-pasta");
+let all4=document.getElementById("nav-salads");
+let all5=document.getElementById("nav-deserts");
+let all6=document.getElementById("nav-pizzas");
+
+all.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+all1.addEventListener("click", function () {
+  window.location.href = "Menu-Burger.html";
+});
+all2.addEventListener("click", function () {
+  window.location.href = "Menu-Drinks.html";
+});
+all3.addEventListener("click", function () {
+  window.location.href = "Menu-Pasta.html";
+});
+all4.addEventListener("click", function () {
+  window.location.href = "Menu-Salads.html";
+});
+all5.addEventListener("click", function () {
+  window.location.href = "Menu-Desert.html";
+});
+all6.addEventListener("click", function () {
+  window.location.href = "Menu-Pizza.html";
+});
+
