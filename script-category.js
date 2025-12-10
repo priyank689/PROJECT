50708c8f2511f9977b08e816a9c2ef9fa2f11d9d
PRIYANSHU <priyank2657@gmail.com>
2025-12-10 20:59:02 +0530

    added a js page

---
+++ script-category.js
@@ -0,0 +1,14 @@
+let all1=document.getElementById("cpb");
+let all2=document.getElementById("cpb1");
+let all3=document.getElementById("cpb2");
+
+all1.addEventListener("click", function () {
+  window.location.href = "category-shop.html";
+});
+
+all2.addEventListener("click", function () {
+  window.location.href = "category-shop2.html";
+});
+all3.addEventListener("click", function () {
+  window.location.href = "category-shop3.html";
+});
